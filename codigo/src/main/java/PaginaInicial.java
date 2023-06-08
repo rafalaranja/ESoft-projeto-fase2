b496690167571255777652d56933eeeda2a8fe35
Rafael Santos <rafaelfsantos03@gmail.com>
2023-06-08 16:20:11 +0100

    Parte estetica do Menu Inicial

--- codigo/src/main/java/PaginaInicial.java
+++ codigo/src/main/java/PaginaInicial.java
@@ -1,9 +1,15 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PaginaInicial extends JFrame {
     private JPanel painelPrincipal;
     private JButton eventosButton;
     private JButton atletasButton;
+    private JButton eventosButton1;
+    private JButton atletasButton1;
+    private JButton estatisticasButton;
+    private JButton sobreButton;
 
     public static void main(String[] args) {
         PaginaInicial paginaInicial = new PaginaInicial();
@@ -15,6 +21,7 @@ public class PaginaInicial extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setContentPane(painelPrincipal);
         pack();
-
     }
+
+
 }
