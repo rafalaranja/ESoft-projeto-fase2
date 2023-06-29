066db6180c4aee408e2b1c2617384a343f68073c
Rafael Santos <rafaelfsantos03@gmail.com>
2023-06-29 02:48:16 +0100

    Adicionar Prova Quase

--- codigo/src/main/java/Prova.java
+++ codigo/src/main/java/Prova.java
@@ -2,6 +2,7 @@ public class Prova {
     private String nome;
     private String genero;
     private String categoriaPeso;
+    private Evento evento;
 
     public Prova(String nome, String genero, String categoriaPeso) {
         this.nome = nome;
